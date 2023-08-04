1d1b0fe37cdfa4d74eba2a587d0831e8d8340a87
premthakral <thakralprem4@gmail.com>
2023-08-04 00:50:42 -0700

    first program of python

---
+++ p1.py
@@ -0,0 +1,5 @@
+a=float(input("enter pincipal value:"))
+b=float(input("enter rate value:"))
+c=float(input("enter time value:"))
+simple_interest=(principle*rate*time)/100
+amount=(simple_interest+principle
